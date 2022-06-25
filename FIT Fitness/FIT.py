a602a3a181b3d2e8ff42e5d77042f4db93d377cb
RUSHABH GALA <88271018+rvgala29@users.noreply.github.com>
2022-06-25 13:20:25 +0530

    Create FIT.py
    This solution is a part of:
        June Long Two 2022 Division 4 (Rated)
    
    Problem Statement:
    Chef wants to become fit for which he decided to walk to the office and return home by walking. It is known that Chef's office is X km away from his home.
    
    If his office is open on 5 days in a week, find the number of kilometers Chef travels through office trips in a week.

---
+++ FIT Fitness/FIT.py
@@ -0,0 +1,8 @@
+# directly iterate over the testcases 
+# Save time and Space by not allocating a variable and assigning it 
+for t in range(int(input())):
+    # Calculate the distance and print the result
+    # No variable for distance as it would require memory
+    # 5 is for number of days Chef goes to office
+    # 2 is becaues he would come and go to office
+    print(5*2*int(input()))
